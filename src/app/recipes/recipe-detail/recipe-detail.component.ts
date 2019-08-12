f4d1fd8bd7a4e6ab060ae8b44d8b7927b6233d25
Damien Hyatt <hongdemian@gmail.com>
2019-08-11 21:18:16 -0600

    layout begins, component files added to repo

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -14,7 +14,8 @@ export class RecipeDetailComponent implements OnInit {
 
   constructor(
     private recipeService: RecipeService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private router: Router
   ) {}
 
   ngOnInit() {
@@ -23,6 +24,9 @@ export class RecipeDetailComponent implements OnInit {
       console.log(this.id);
       this.recipe = this.recipeService.getRecipe(this.id);
     });
+  }
+  onEditRecipe() {
+    this.router.navigate(['../', this.id, 'edit'], { relativeTo: this.route });
   }
   onAddToShoppingList() {
     this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
